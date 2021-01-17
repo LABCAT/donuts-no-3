64a6a90289a7614f60266f6b6c9d75414218e3e5
LABCAT <shane.watters.nz@gmail.com>
2021-01-17 19:31:18 +1300

    Updating sketch to be full screen and adding colour scheme and audio.

--- src/js/P5Sketch.js
+++ src/js/P5Sketch.js
@@ -19,7 +19,7 @@ const P5Sketch = () => {
 
         p5.song = null;
 
-        p5.tempo = 101;
+        p5.tempo = 97;
 
         p5.time = 0;
 
@@ -30,54 +30,65 @@ const P5Sketch = () => {
         p5.particlesArray = [];
 
         p5.setup = () => {
-            //p5.song = p5.loadSound(audio);
-            //p5.colorMode(p5.HSB);
-            //p5.canvas = p5.createCanvas(p5.canvasWidth, p5.canvasHeight); 
+            p5.song = p5.loadSound(audio);
+            p5.colorMode(p5.HSB, 360, 100, 100, 100);
+            p5.canvas = p5.createCanvas(p5.canvasWidth, p5.canvasHeight); 
             //p5.background(0,0,94);
             p5.smooth();
 
-            p5.canvas = p5.createCanvas(600, 600);
-            p5.noStroke();
+            //p5.canvas = p5.createCanvas(600, 600);
+            p5.noFill();
+            p5.strokeWeight(2);
 
         };
 
         p5.draw = () => {
-            let currentBar = p5.getSongBar();
-            if (p5.song._lastPos > 0 && currentBar >= 0 && p5.song.isPlaying()) {
+            let currentBeat = p5.getSongBeat();
+            // if (p5.song._lastPos > 0 && currentBar >= 0 && p5.song.isPlaying()) {
 
 
-            }
+            // }
+            if (p5.song.isPlaying()) {
+                p5.background(10, 1); // translucent background (creates trails)
+                let hue = 0;
+                let circleSize = 180 - (p5.time * 10);
+                circleSize = circleSize > 30 ? circleSize : 30;
+                circleSize = 45 + p5.time;
 
-            p5.background(10, 10); // translucent background (creates trails)
-            p5.fill(40, p5.time, 40);
+                // make a x and y grid of ellipses
+                for (let x = 0; x <= p5.canvasWidth; x = x + circleSize) {
+                    for (let y = 0; y <= p5.canvasHeight; y = y + circleSize) {
+                        hue = (y % 24) * 15;
+                        p5.stroke(hue, 100, 100, 50);
+                        // starting point of each circle depends on mouse position
+                        const xAngle = p5.map(p5.canvasWidth, 0, p5.canvasWidth, -4 * p5.PI, 4 * p5.PI, true);
 
-            // make a x and y grid of ellipses
-            for (let x = 0; x <= 600; x = x + 30) {
-                for (let y = 0; y <= 600; y = y + 30) {
-                    // starting point of each circle depends on mouse position
-                    const xAngle = p5.map(p5.mouseX, 0, p5.width, -4 * p5.PI, 4 * p5.PI, true);
+                        const yAngle = p5.map(p5.canvasHeight, 0, p5.canvasHeight, -4 * p5.PI, 4 * p5.PI, true);
+                        // // and also varies based on the particle's location
+                        const angle = xAngle * (x / p5.canvasWidth) + yAngle * (y / p5.canvasHeight);
 
-                    const yAngle = p5.map(p5.mouseY, 0, p5.height, -4 * p5.PI, 4 * p5.PI, true);
-                    // // and also varies based on the particle's location
-                    const angle = xAngle * (x / p5.width) + yAngle * (y / p5.height);
-
-                    // // each particle moves in a circle
-                    const myX = x + 20 * p5.cos(2 * p5.PI * p5.time + angle);
-                    const myY = y + 20 * p5.sin(2 * p5.PI * p5.time + angle);
-                    p5.ellipse(myX, myY, 10); // draw particle
+                        // // each particle moves in a circle
+                        const myX = x + 20 * p5.cos(2 * p5.PI * p5.time + angle);
+                        const myY = y + 20 * p5.sin(2 * p5.PI * p5.time + angle);
+                        p5.ellipse(myX, myY, circleSize/3); // draw particle
+                    }
                 }
+
+                if (currentBeat != p5.previousBeat){
+                    p5.previousBeat = currentBeat;
+                    p5.time = p5.time + (0.225 / 8); // update time
+                    console.log('// update time', p5.time);
+                    console.log('// lsast', p5.song._lastPos);
+                }
+                
             }
-
-            p5.time = p5.time + 0.01; // update time
-            console.log('// update time', p5.time);
-
         };
 
         p5.beatPerBar = 4;
 
         p5.getSongBeat = () => {
             if (p5.song && p5.song.buffer) {
-                const barAsBufferLength = (p5.song.buffer.sampleRate * 60 / p5.tempo);
+                const barAsBufferLength = (p5.song.buffer.sampleRate * 60 / p5.tempo) / 8;
                 return Math.floor(p5.song._lastPos / barAsBufferLength) + 1;
             }
             return -1;
@@ -93,13 +104,13 @@ const P5Sketch = () => {
         }
 
         p5.mousePressed = () => {
-            // if (p5.song.isPlaying()) {
-            //     p5.song.pause();
-            // } else {
-            //     document.getElementById("play-icon").classList.add("fade-out");
-            //     p5.canvas.addClass('fade-in');
-            //     p5.song.play();
-            // }
+            if (p5.song.isPlaying()) {
+                p5.song.pause();
+            } else {
+                document.getElementById("play-icon").classList.add("fade-out");
+                p5.canvas.addClass('fade-in');
+                p5.song.play();
+            }
             p5.createParticle = true;
         };
 
